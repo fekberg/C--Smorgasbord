e671e6b1fd07ac16f682d43684ad595a83501fb4
Filip Ekberg <mail@filipekberg.se>
2012-03-17 16:47:22 +0100

    Updated the chapter 5  example with application dispatcher

--- Chapter 5/BackgroundWorkerDemo/BackgroundWorkerDemo/MainWindow.xaml.cs
+++ Chapter 5/BackgroundWorkerDemo/BackgroundWorkerDemo/MainWindow.xaml.cs
@@ -2,13 +2,14 @@
 using System.Windows.Controls;
 using System.ComponentModel;
 using System.Threading;
+using System;
 
 namespace BackgroundWorkerDemo
 {
     public partial class MainWindow : Window
     {
         private BackgroundWorker _worker = new BackgroundWorker();
-
+        private TextBox _result = new TextBox();
         public MainWindow()
         {
             InitializeComponent();
@@ -24,6 +25,7 @@ namespace BackgroundWorkerDemo
 
             var panel = new StackPanel();
             panel.Children.Add(button);
+            panel.Children.Add(_result);
 
             Content = panel;
 
@@ -32,6 +34,8 @@ namespace BackgroundWorkerDemo
         void worker_DoWork(object sender, DoWorkEventArgs e)
         {
             Thread.Sleep(2000);
+
+            Application.Current.Dispatcher.Invoke(new Action(() => { _result.Text = DateTime.Now.ToString(); }));
         }
 
         void button_Click(object sender, RoutedEventArgs e)
