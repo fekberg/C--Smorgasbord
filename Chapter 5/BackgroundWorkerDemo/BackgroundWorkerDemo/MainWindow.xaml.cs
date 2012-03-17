f4f584d1e9dc40d4c77b681e003b306df4c4b288
Filip Ekberg <mail@filipekberg.se>
2012-03-17 16:26:45 +0100

    Added Chapter 5 samples

---
+++ Chapter 5/BackgroundWorkerDemo/BackgroundWorkerDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Threading;
+
+namespace BackgroundWorkerDemo
+{
+    public partial class MainWindow : Window
+    {
+        private BackgroundWorker _worker = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _worker.DoWork += worker_DoWork;
+
+            var button = new Button
+            {
+                Content = @"Process data",
+            };
+
+            button.Click += button_Click;
+
+            var panel = new StackPanel();
+            panel.Children.Add(button);
+
+            Content = panel;
+
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(2000);
+        }
+
+        void button_Click(object sender, RoutedEventArgs e)
+        {
+            _worker.RunWorkerAsync();
+        }
+    }
+}
---
+++ Chapter 5/README.md
@@ -0,0 +1,7 @@
+A C# Smorgasbord - Code Samples for Chapter 5
+=============================================
+
+### What do I need to run the samples?
+
+## Async CTP
+* Install the Async CTP, you can download that for free here: http://msdn.microsoft.com/en-us/vstudio/gg316360
